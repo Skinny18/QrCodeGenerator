25c6530c1f45faa0613a581b9d8e3d09f8fde0fc
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2022-09-16 14:05:56 -0400

    generator

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
-export const api = "http://localhost:5000/"
+export const api = "https://qrcodegenera.herokuapp.com/"
